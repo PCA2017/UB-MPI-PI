c2bfd25e5ba521ac39fa1fe972a28048d9cc1b61
- <->
2017-12-13 15:38:16 +0100

    Adapted list calc

--- src/calculate_MPI_pi.cpp
+++ src/calculate_MPI_pi.cpp
@@ -42,7 +42,7 @@ int main(int argc, char **argv) {
 	// Calculate the Starts and Ends of the segments the different Tasks calculate
 	int start = 0;
 	int end;
-	for(i = 1; i < num_tasks; i++)
+	for(i = 1; i <= (num_tasks-1); i++)
 	{
 		end = round((double(n) / double(num_tasks)) * double(i + 1));
 		vs[i] = start;
